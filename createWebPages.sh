e0bdaf14c5194be12e078d1f1a5b72287617c7df
Danielle <dan.thurow@gmail.com>
2020-12-04 10:18:00 -0600

    Include the templates and bash script and initial data that generates the website, move website to live in docs folder

---
+++ createWebPages.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+OIFS="$IFS"
+IFS=$'\n'
+#pull in the templates for creating the individual posts and the home page
+postTemplate=$(cat ./postFormat.html)
+indexTemplate=$(cat ./indexFormat.html)
+imageTemplate=$(cat ./imageFormat.html)
+linkTemplate=$(cat ./indexLinkFormat.html)
+
+#the folder name to publish the site to
+websiteFolder="docs"
+
+index=$indexTemplate
+#delete old copy of the website
+if [ -d "$websiteFolder" ]; then
+    rm -r $websiteFolder
+fi
+
+#make folder structure again for website
+mkdir $websiteFolder
+mkdir $websiteFolder/images
+
+#copy all css
+for i in $(ls *.css)
+do
+    cp $i $websiteFolder
+done
+
+#copy all jpg images
+for i in $(ls *.jpg)
+do
+    cp $i $websiteFolder
+done
+
+
+#loop through posts and create the individual pages
+for i in $(ls -cd */)
+do 
+    if [ $i = "$websiteFolder/" ]; then
+        continue
+    fi
+
+    description=$(cat $i/description.txt)
+    
+    images=""
+    #loop through the images and add them into the post template
+    for image in $(cd $i && ls *.jpg)
+    do
+        imagePiece=${imageTemplate//\{src\}/"images/$image"}
+        images="$images $imagePiece"
+        cp "$i/$image" "$websiteFolder/images"
+    done
+
+    #add the description
+    post=${postTemplate//\{description\}/$description}
+    #add the photos
+    post=${post/\{images\}/$images}
+
+    #print out the post into its own html file
+    echo $post > "$websiteFolder/$(basename $i) post.html"
+
+    #add a link to this page on the index page
+    link=${linkTemplate//\{page\}/"$(basename $i) post.html"}
+    link=${link//\{plant\}/"$(basename $i)"}
+    index=${index//\{link\}/$link}
+    
+done
+
+#get rid of the final trailing {link} and write it out to a file
+index=${index//\{link\}/""}
+echo $index > "$websiteFolder/index.html"
+
+IFS="$OIFS"
